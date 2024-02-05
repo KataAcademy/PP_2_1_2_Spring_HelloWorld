48cc183d95fb17e40f123e57f3e14e4a21758ca5
Senz One <sem63sam@yandex.ru>
2024-02-05 22:09:55 +0400

    the task is completed added: cat class and ReadMe.md

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld helloWorld2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(helloWorld1.getMessage());
+        System.out.print(helloWorld1.equals(helloWorld2) + "\n");
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(cat1.getMessage());
+        System.out.println(cat1.equals(cat2));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+import java.util.Objects;
+
+public class Cat {
+    private String message;
+
+    public Cat() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
